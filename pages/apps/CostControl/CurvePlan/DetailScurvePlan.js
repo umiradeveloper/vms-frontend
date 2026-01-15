a8a9c3d0e246167b678ef04bfae2105e6cd706fa
hafid hanafi <hafidhanafid@gmail.com>
2026-01-15 14:49:15 +0700

    plan action & s curve

---
+++ pages/apps/CostControl/CurvePlan/DetailScurvePlan.js
@@ -0,0 +1,386 @@
+import { Fragment, useEffect, useState } from "react";
+import Seo from "@/shared/layout-components/seo/seo";
+import PageHeaderVms from "../../Component/PageHeaderVms";
+import LoadersSimUmira from "../../Component/LoaderSimUmira";
+import { Card, Col, Row } from "react-bootstrap";
+import apiConfig from "@/utils/AxiosConfig";
+import { useSearchParams } from "next/navigation";
+import CreateScurvePlan from "./modals/CreateScurvePlan";
+import { useRouter } from "next/router";
+import BasicTableCostControl from "../../DataTables/DataTablesCostControl";
+import DetailDokumenScurvePlan from "./modals/DetailDokumenScurvePlan";
+import Swal from "sweetalert2";
+
+
+
+const DetailScurvePlan = () => {
+    const [loader, setLoader] = useState(false);
+    const [dataTable, setDataTable] = useState([]);
+    const [reload, setReload] = useState(false);
+    const [openModalCreate, setOpenModalCreate] = useState({
+        id_proyek: "",
+        open_modal: false
+    })
+    const [openDetailDokumen, setOpenDetailDokumen] = useState({
+        open: false,
+        file_url: ""
+    });
+    const navigate = useRouter();
+     const params = useSearchParams();
+    const [dataProyek, setDataProyek] = useState({
+        total_bk: "",
+        total_pu: "",
+        kode_proyek: "",
+        nama_proyek: "",
+        deskripsi_proyek: "",
+        tanggal_kontrak: "",
+        tanggal_awal_kontrak: "",
+        nominal_mos:"",
+        biaya_rap: "",
+        biaya_rab: "",
+        bk_pu_awal: ""
+    });
+    const COLUMNS = [
+         {
+            Header: "Week",
+            accessor: "week",
+        },
+        {
+            Header: "Rentang Tanggal",
+            accessor: "tanggal",
+        },
+        {
+            Header: "Nominal Scurve",
+            accessor: "nominal_scurve",
+        },
+        {
+            Header: "Dokumen Pendukung",
+            accessor: "dokumen_pendukung",
+        },
+        {
+            Header: "Aksi",
+            accessor: "aksi",
+        },
+    ]
+    const toCurrency = (value) => {
+        if (!value) return "Rp0";
+
+        return new Intl.NumberFormat("id-ID", {
+            style: "currency",
+            currency: "IDR",
+            minimumFractionDigits: 0
+        }).format(Number(value));
+    };
+
+    const formatPercent = (value, digits = 2) => `${Number(value).toLocaleString(undefined, { minimumFractionDigits: digits, maximumFractionDigits: digits })}%`;
+    const calcPercentage = (part, total) => {
+        const t = Number(total) || 0;
+        if (t === 0) return 0;
+        const p = (Number(part) / t) * 100;
+        return p;
+    }
+    const getDataById = async () => {
+        setLoader(true);
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+        try {
+            const result = await apiConfig.get(apiUrl + "/CostControl/Proyek/get-proyek-id-bk-pu?id=" + params.get("id"), {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + localStorage.getItem("token")
+                }
+            });
+            console.log(result);
+            if (result.status) {
+                setDataProyek({
+                    total_bk: result.data.data.total_bk,
+                    total_pu: result.data.data.total_pu,
+                    nama_proyek: result.data.data.proyek.nama_proyek,
+                    kode_proyek: result.data.data.proyek.kode_proyek,
+                    nominal_mos: result.data.data.current_mos,
+                    deskripsi_proyek: result.data.data.proyek.deskripsi_proyek,
+                    tanggal_kontrak: (result.data.data.proyek.tanggal_akhir_kontrak) ? result.data.data.proyek.tanggal_akhir_kontrak : "",
+                    tanggal_awal_kontrak: (result.data.data.proyek.tanggal_awal_kontrak) ? result.data.data.proyek.tanggal_awal_kontrak : "",
+                    biaya_rap: (result.data.data.proyek.biaya_rap) ? result.data.data.proyek.biaya_rap : "",
+                    biaya_rab: (result.data.data.proyek.biaya_rab) ? result.data.data.proyek.biaya_rab : "",
+                    bk_pu_awal: (result.data.data.proyek.bk_pu_awal) ? result.data.data.proyek.bk_pu_awal + " %" : ""
+                });
+
+            }
+            setLoader(false)
+            console.log(result)
+        } catch (error) {
+            setLoader(false)
+            console.log("e = " + error);
+        }
+    }
+    const getFile = async(id) => {
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+        // console.log(id);
+            setLoader(true);
+            try {
+                const response = await apiConfig.get(apiUrl+"/CostControl/Scurve/dokumen-file", {
+                    responseType: "blob",
+                    headers:{
+                        "Content-Type":"application/json",
+                        "Authorization": "Bearer "+localStorage.getItem("token")
+                    },
+                    params:{
+                        id: id
+                    }
+                });
+                // const typeData = {};
+                // typeData.type = "application/pdf";
+                const data = response.data;
+                
+                if(data){
+                    // console.log(data);
+                    const fileURL = window.URL.createObjectURL(
+                        new Blob([data], {type: "application/pdf"})
+                    );
+                    //setFrameSrc(fileURL);
+                    setOpenDetailDokumen({
+                        file_url: fileURL, open: true
+                    });
+                    // setSelectedOptions(kualifikasiArr);
+                    // setTableData(userArr);
+                    setLoader(false);
+                }
+                
+            } catch (error) {
+                console.log(error);
+                // setError(error.message);
+                setLoader(false);
+            }
+       }
+    const getDataScurve = async() => {
+        setLoader(true);
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+        try {
+            const result = await apiConfig.get(apiUrl + "/CostControl/Scurve/get-scurve-by-proyek?id_proyek=" + params.get("id"), {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + localStorage.getItem("token")
+                }
+            });
+            if(result.status == 200){
+                const arrScurve = [];
+                 for (const a of result.data.data) {
+                    arrScurve.push({
+                        id_scurve: a.id_scurve,
+                        week: a.week,
+                        tanggal: a.tanggal_awal + " s/d " + a.tanggal_akhir,
+                        nominal_scurve: toCurrency(a.nominal_scurve),
+                        dokumen_pendukung: (
+                            <div className="d-flex gap-2">
+                                <button
+                                    className="btn btn-sm btn-info"
+                                    onClick={() =>
+                                        {getFile(
+                                            a.id_scurve
+                                        );
+                                        //setOpenDetailDokumen({...openDetailDokumen, open:true});
+                                    }
+                                    }
+                                >
+                                    Lihat Dokumen
+                                </button>
+                            </div>
+                        ),
+                        aksi: <div className="d-flex gap-2">
+                            <button
+                                className="btn btn-sm btn-danger"
+                                onClick={() => { deleteData(a.id_scurve) }}
+                            >
+                                Delete
+                            </button>
+                        </div>,
+                    })
+                 }
+                 setDataTable(arrScurve);
+                 setLoader(false);
+            }
+        } catch (error) {
+            setLoader(false);
+            console.log("e = "+error);
+        }
+
+    }
+    const deleteData = async(id) => {
+        const resultConfirm = await AlertConfirm("Apakah anda yakin ingin menghapus data ini ? ", "warning", "Hapus", false, "Data berhasil di hapus");
+        if(resultConfirm.status){
+            setLoader(true);
+            const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+            try {
+                const result = await apiConfig.delete(apiUrl + "/CostControl/Scurve/delete-scurve-by-id?id="+id, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + localStorage.getItem("token")
+                    }
+                });
+                if(result.status == 200){
+                    setLoader(false);
+				    swalAlert(result.data.message, result.statusText, "success");
+                }
+                
+            } catch (error) {
+                // console.log(error);
+                setLoader(false);
+			    swalAlert(error.message, error.code, "error");
+            }
+            
+
+        }
+        // console.log(id)
+    }
+    const swalAlert = (message, title, icon) => {
+            let timerInterval;
+    
+            Swal.fire({
+                title: title,
+                html: message,
+                icon: icon,
+                timer: 5000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading();
+                },
+                willClose: () => {
+                    clearInterval(timerInterval);
+                },
+            }).then((result) => {
+                /* Read more about handling dismissals below */
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    console.log("I was closed by the timer");
+                }
+            });
+        }
+    const AlertConfirm = async(message, icon, confirmButtonName, textarea = false, messageDeleted = "Your file has been deleted.") => {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger me-2"
+            },
+            buttonsStyling: false
+        });
+        let objSwall = {
+            title: "Apakah Yakin?",
+            text: message,
+            icon: icon,
+            showCancelButton: true,
+            confirmButtonText: confirmButtonName,
+            cancelButtonText: "Kembali",
+            reverseButtons: true,
+           
+        };
+        
+        if(textarea){
+            objSwall.input = 'textarea';
+            objSwall.inputLabel = 'Catatan';
+            objSwall.inputPlaceholder = 'Catatan....';
+        
+        }
+        const result = await swalWithBootstrapButtons.fire(objSwall);
+        if (result.isConfirmed) {
+                setReload(prev => !prev);
+                return {
+                    status: true,
+                    value: result.value
+                }; 
+                    // ✅ user confirmed
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            // await swalWithBootstrapButtons.fire(
+            //     "Cancelled",
+            //     // "Your imaginary file is safe :)",
+            //     "error"
+            // );
+            return {
+                status: false,
+                // value: result.value
+            }; // ✅ user cancelled
+        }
+
+        return false;
+    }
+    useEffect(() => {
+        if (params.get("id")) {
+            getDataById();
+            getDataScurve();
+            // getPuByProyek();
+
+        }
+    },[params.get("id"), reload, openModalCreate]);
+
+    return(
+        <Fragment>
+            <Seo title={"Detail S Curve"} />
+
+            <PageHeaderVms title='S Curve' item='Daftar S Curve' active_item='Detail S Curve' />
+            <LoadersSimUmira open={loader} />
+            <CreateScurvePlan openModal={openModalCreate} setOpenModal={setOpenModalCreate} loader={loader} setLoader={setLoader} />
+            <DetailDokumenScurvePlan openModal={openDetailDokumen} setOpenModal={setOpenDetailDokumen} />
+            <Row>
+                <Col xl={12}>
+                    <Card className="custom-card">
+                        <Card.Header className="d-flex align-items-center justify-content-between">
+                            <button
+                                type="button" className="btn btn-warning label-btn rounded-pill"
+                                onClick={() => navigate.push("/apps/CostControl/CurvePlan/DaftarScurvePlan")}
+                            >
+                                <i className="ri-arrow-left-line label-btn-icon me-2 rounded-pill" />
+                                kembali
+                            </button>
+                            <button
+                                type="button" className="btn btn-primary label-btn rounded-pill"
+                                // onClick={() => setOpenModalUpload({ id_proyek: params.get("id"), open_modal: true })}
+                                onClick={() => setOpenModalCreate({id_proyek: params.get("id"),open_modal: true})}
+                            // onClick={() => console.log("test")}
+                            // onClick={() => navigate.push("/apps/CostControl/Rapa/DaftarRapa")}
+                            >
+                                <i className="ri-add-circle-line label-btn-icon me-2 rounded-pill" />
+                                Input S curve
+                            </button>
+
+                        </Card.Header>
+                        <Card.Body>
+                            <h5>Kode Proyek : {dataProyek.kode_proyek}</h5>
+                            <h5>Nama Proyek : {dataProyek.nama_proyek}</h5>
+                            <h5>Tanggal Awal Kontrak : {dataProyek.tanggal_awal_kontrak}</h5>
+                            <h5>Tanggal Berakhir Kontrak : {dataProyek.tanggal_kontrak}</h5>
+                            <h5>RAB (Rincian Anggaran Biaya) : {toCurrency(dataProyek.biaya_rab)}</h5>
+                            <h5>RAP (Rincian Anggaran Proyek) : {toCurrency(dataProyek.biaya_rap)}</h5>
+                            <h5>Pendapatan Usaha : {toCurrency(dataProyek.total_pu)}</h5>
+                            <h5>Posisi Biaya Konstruksi : {toCurrency(dataProyek.total_bk)}</h5>
+                            <h5>Material On Site: {toCurrency(dataProyek.nominal_mos)}</h5>
+                            <h5>BK / PU Awal : {dataProyek.bk_pu_awal}</h5>
+                            <h5>Persentase BK/PU : {formatPercent(calcPercentage(dataProyek.total_bk, dataProyek.total_pu))}</h5>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col xl={12}>
+                    <Card className="custom-card">
+                        <Card.Header>
+                            <div className="card-title">Detail S Curve </div>
+                        </Card.Header>
+
+                        <Card.Body>
+                            {(dataTable).length === 0 ? (
+                                <p className="text-muted">Belum ada data</p>
+                            ) : (
+                                <div className="table-responsive">
+                                    <BasicTableCostControl column={COLUMNS} datatable={dataTable} />
+                                </div>
+                            )}
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Fragment>
+    );
+}
+
+DetailScurvePlan.layout = "ContentlayoutVms";
+
+export default DetailScurvePlan;
---
+++ pages/apps/CostControl/CurvePlan/modals/CreateScurvePlan.js
@@ -0,0 +1,170 @@
+import { Button, Col, Modal, Row } from "react-bootstrap";
+
+import { FilePond, registerPlugin } from "react-filepond";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
+import apiConfig from "@/utils/AxiosConfig";
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
+
+registerPlugin(FilePondPluginFileValidateType, FilePondPluginFileValidateSize);
+
+
+const CreateScurvePlan = ({openModal, setOpenModal, loader, setLoader}) => {
+    const [daftarWeek, setDaftarWeek] = useState([]);
+    const [dokumenFiles, setDokumenFiles] = useState();
+    const [dataCreate, setDataCreate] = useState({
+        week_scurve:"",
+        nominal_scurve:""
+    })
+    const getDaftarWeek = async () => {
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+        try {
+            const result = await apiConfig.get(
+                `${apiUrl}/master/get-week-by-project?id_project=${openModal.id_proyek}`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + localStorage.getItem("token")
+                    }
+                }
+            );
+            console.log(result);
+            if (result.status === 200) {
+                setDaftarWeek(result.data.data); // <- simpan array wee
+            }
+        } catch (error) {
+            console.log("Error fetching weeks:", error);
+        }
+    };
+
+    const submit = async() => {
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+        setLoader(true);
+        const splitweek = dataCreate.week.split("|");
+        const valueNominalPuClean = (dataCreate.nominal_scurve) ? dataCreate.nominal_scurve.replace(/\./g, "") : "";
+        const formData = new FormData();
+        formData.append("nominal_scurve", valueNominalPuClean);
+        formData.append("id_proyek", openModal.id_proyek);
+        formData.append("week", splitweek[0]);
+        formData.append("tanggal_awal", splitweek[1]);
+        formData.append("tanggal_akhir", splitweek[2]);
+        formData.append("file_dokumen", dokumenFiles[0].file);
+        try {
+            const result = await apiConfig.post(apiUrl + "/CostControl/Scurve/create-scurve", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    "Authorization": "Bearer " + localStorage.getItem("token")
+                }
+            });
+            if (result.status == 200) {
+                setLoader(false);
+                swalAlert(result.data.message, result.statusText, "success");
+                setOpenModal({ ...openModal, open_modal: false });
+                // console.log(result)
+            }
+        } catch (error) {
+            
+        }
+    }
+    const swalAlert = (message, title, icon) => {
+        let timerInterval;
+
+        Swal.fire({
+            title: title,
+            html: message,
+            icon: icon,
+            timer: 5000,
+            timerProgressBar: true,
+            didOpen: () => {
+                Swal.showLoading();
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+            },
+        }).then((result) => {
+            /* Read more about handling dismissals below */
+            if (result.dismiss === Swal.DismissReason.timer) {
+                console.log("I was closed by the timer");
+            }
+        });
+    }
+    const handleNominalScurve = (e) => {
+        let val = e.target.value.replace(/[^\d]/g, ""); // hanya angka
+        val = val ? new Intl.NumberFormat("id-ID").format(val) : "";
+        setDataCreate({ ...dataCreate, nominal_scurve: val })
+        // setValueRab(val);
+    };
+    useEffect(() => {
+        getDaftarWeek()
+    },[openModal.open_modal])
+    return(
+        <Modal size="md" show={openModal.open_modal} onHide={() => setOpenModal({ ...openModal, open_modal: false })} className="fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <Modal.Header closeButton>
+                <h6 className="modal-title" id="exampleModalLabel">Tambah S Curve</h6>
+            </Modal.Header>
+            <Modal.Body className="">
+                <Row>
+                    <Col xl={12}>
+                        <Row>
+                            <Col xl={12} className="rounded-3">
+                                <div className="row gy-2 pb-3">
+                                    <Col x1={12}>
+                                        <label className="form-label mt-3">
+                                            Pilih Week <span style={{ color: "red" }}>*</span> :
+                                        </label>
+
+                                        <select
+                                            className="form-select"
+                                            value={dataCreate.week}
+                                            onChange={(e) => setDataCreate({ ...dataCreate, week: e.target.value })}
+                                        >
+                                            <option value="">-- Pilih Week --</option>
+
+                                            {daftarWeek.map((item, index) => (
+                                                <option key={index} value={item.week + "|" + item.startDate + "|" + item.endDate}>
+                                                    Week {item.week} ({item.startDate} s/d {item.endDate})
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </Col>
+                                    <Col xl={12}>
+                                        <label htmlFor="pendapatan-usaha" className="form-label ">Nominal S Curve <span style={{ color: "red" }}>*</span> :</label>
+                                        <input type="text" className={`form-control`} id="nominal_pu" placeholder="Nominal S Curve" onChange={handleNominalScurve} value={dataCreate.nominal_scurve} />
+                                    </Col>
+                                    <Col xl={12}>
+                                        <label className="form-label mt-3">
+                                            Upload Dokumen Pendukung <span style={{ color: "red" }}>*</span> :
+                                        </label>
+
+                                        <FilePond
+                                            className="filepond-custom"
+                                            name="files"
+                                            acceptedFileTypes={['application/pdf']}
+                                            maxFileSize="5MB"
+                                            labelIdle='Drag & Drop file atau klik'
+                                            labelMaxFileSizeExceeded="File terlalu besar"
+                                            labelMaxFileSize="Maksimal ukuran file: 5MB"
+                                            files={dokumenFiles}
+                                            onupdatefiles={setDokumenFiles}
+                                        />
+                                    </Col>
+                                </div>
+                            </Col>
+                        </Row>
+                       
+                    </Col>
+                </Row>
+            </Modal.Body>
+            <Modal.Footer className="">
+                <Button variant='contained' type="button" className="btn btn-primary"
+                    data-bs-dismiss="modal" onClick={submit}>Tambah</Button>
+                <Button variant='contained' type="button" className="btn btn-secondary"
+                    data-bs-dismiss="modal" onClick={() => setOpenModal({ ...openModal, open_modal: false })}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    )
+
+}
+export default CreateScurvePlan;
