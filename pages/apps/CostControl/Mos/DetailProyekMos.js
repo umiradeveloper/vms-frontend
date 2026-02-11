83777a785b3c376c041fb01a071620f56579608a
umiradeveloper <umiradeveloper@gmail.com>
2026-02-11 11:25:34 +0800

    persentase bkpu

--- pages/apps/CostControl/Mos/DetailProyekMos.js
+++ pages/apps/CostControl/Mos/DetailProyekMos.js
@@ -370,7 +370,7 @@ const DetailProyekMos = () => {
                                 {toCurrency(calcBkAfterMos(dataProyek.total_bk, dataProyek.nominal_mos))}
                             </h5>
                             <h5>BK / PU Awal : {dataProyek.bk_pu_awal}</h5>
-                            <h5>Persentase BK/PU : {formatPercent(calcPercentage(dataProyek.total_bk, dataProyek.total_pu))}</h5>
+                            <h5>Persentase BK/PU : {formatPercent(calcPercentage((calcBkAfterMos(dataProyek.total_bk, dataProyek.nominal_mos)), dataProyek.total_pu))}</h5>
                         </Card.Body>
                     </Card>
                 </Col>
